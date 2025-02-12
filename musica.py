bae974bc919c8ddd4ca6c9a4662f92db1a17a4e1
Pedro Kurtz <Pedro Kurtz@urca.britishschool.g12.br>
2025-02-12 10:50:04 -0300

    links do forms para cadastro de músicas concluído

--- musica.py
+++ musica.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Musica:
     def __init__(self, nome, cantorBandaGrupo, genero):
         self.nome = nome
         self.cantorBanda = cantorBandaGrupo
         self.genero = genero
+
+musica01 = Musica('Temporal', 'Hungria', 'Rap')
+musica02 = Musica('Papai banca', 'Mc Ryan SP', 'Funk')
+musica03 = Musica('Camisa 10', 'Turma do Pagode', 'Pagode')
+
+lista = [musica01, musica02, musica03]
 
 app = Flask(__name__)
 
@@ -15,15 +21,28 @@ def hello():
 @app.route('/musicas')
 def listarMusicas():
 
-    musica01 = Musica('Temporal', 'Hungria', 'Rap')
-    musica02 = Musica('Papai banca', 'Mc Ryan SP', 'Funk')
-    musica03 = Musica('Camisa 10', 'Turma do Pagode', 'Pagode')
-
-    lista = [musica01, musica02, musica03]
-
     return render_template('lista_musica.html',
                            titulo = 'Aprendendo do início com Pedro Kurtz',
                            musicas = lista)
 
+
+
+@app.route('/cadastrar')
+def cadastrarMusicas():
+    return render_template('cadastro_musicas.html')
+
+@app.route('/add', methods=['POST',])
+def adicionarMusicas():
+    nome = request.form['txtNome']
+    cantor = request.form['txtCantor']
+    genero = request.form['txtGenero']
+
+    novaMusica = Musica(nome, cantor, genero)
+
+    lista.append(novaMusica)
+
+    return render_template('lista_musica.html', 
+                            titulo = 'Aprendendo Flask',
+                            musicas = lista)
 
 app.run(debug=True)
